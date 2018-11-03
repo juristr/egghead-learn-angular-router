7e76d9d2cdbcb640c4f98aaa5fbd65dda75a9a18
Juri <juri.strumpflohner@gmail.com>
2018-11-03 22:09:04 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(public authService: AuthService) {}
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _isLoggedIn = false;
+
+  constructor() {}
+
+  isLoggedIn() {
+    return this._isLoggedIn;
+  }
+
+  login() {
+    this._isLoggedIn = true;
+  }
+
+  logout() {
+    this._isLoggedIn = false;
+  }
+}
